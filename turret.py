438bb4a14886778b3cca3357942b5f32ee78608e
Benjamin Nguyen <55600933+Benjycn@users.noreply.github.com>
2025-11-19 17:03:30 -0500

    Update turret.py

--- turret.py
+++ turret.py
@@ -38,7 +38,7 @@ class Stepper:
     seq = [0b0001,0b0011,0b0010,0b0110,
            0b0100,0b1100,0b1000,0b1001]
 
-    delay = 8000
+    delay = 3000
     steps_per_degree = 1024 / 360   # 1:16 gearbox
 
     def __init__(self, sh, lock, index, shared_angles):
